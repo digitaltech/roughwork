60ec895530046e4574258d2c2b8ea16ffced8359
unknown <sun.1095@email.com>
2014-07-30 18:55:18 +0530

    added the testfr directory that contains the intial files for the user signup

---
+++ testfr/signup.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style/signup.css" />
+    </head>
+    <body >
+        <div class = "singupbody">
+            
+            <div class = "signupFormDiv">
+                <form method = "post" >
+                    <input class = "signupInp" type = "text" /><br>
+                    <input class = "signupInp" type = "text" /><br>
+                    <input class = "signupInp" type = "text" /><br>
+                    <input class = "signupInp" type = "text" /><br>
+                    <input class = "signupInp" type = "text" /><br>
+                    <input class = "signupInp" type = "text" /><br>
+                    <input class = "signupInp" type = "text" /><br>
+
+                                        
+                </form>
+            </div>
+            
+        </div>
+
+    </body>
+</html>
