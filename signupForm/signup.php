2b0a3da98cb767dc4c0595562a42c9e0fa7d8e23
unknown <sun.1095@email.com>
2014-07-31 17:05:19 +0530

    First Stage of signup form finished

---
+++ signupForm/signup.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style/signup.css" />
+    </head>
+    <body >
+        <div class = "singupbody">
+            
+            <div class = "signupFormDiv">
+                <form method = "post" >
+                    <input class = "signupInp" type = "text" placeholder = "First Name" /><br>
+                    <input class = "signupInp" type = "text" placeholder = "Last Name" /><br>
+                    <input class = "signupInp" type = "text" placeholder = "Email id" /><br>
+                    <input class = "signupInp" type = "password" placeholder = "Your Password" /><br>
+                    <input class = "signupInp" type = "password" placeholder = "Confirm Password"/><br>
+                    <div class = "signupSelc">
+                        <span>DOB:</span>
+                    <select ><option>Day</option>   </select>
+                    <select ><option>Month</option></select>
+                    <select ><option>Year</option></select>
+                    </div>
+                    <input class = "signupInp" type = "text" placeholder = "Phone number"/><br>
+                    <div class = "signupRad">
+                        <input name = "signUpGend" type = "radio" />Male
+                        <input name = "signUpGend" type = "radio" />Female
+                    </div>
+                    <input class = "signupInp" type = "submit" />
+
+                                        
+                </form>
+            </div>
+            
+        </div>
+
+    </body>
+</html>
